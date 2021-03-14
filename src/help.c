c6553005293cba0bf2d0b1a8784de0e580e0a3e6
Nicholas Godfrey <nickg_8128@outlook.com>
2021-03-14 15:44:54 +0000

    Added copyright notice to some files that I forgot about.

--- src/help.c
+++ src/help.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright © 2021 Nicholas Godfrey
+ *
+ * This file is part of Lossy Text Compressor.
+ *
+ * Lossy Text Compressor is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Public License Version 3 as published
+ * by the Free Software Foundation.
+ *
+ * Lossy Text Compressor is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 #include "help.h"
 
 void help(void) {
